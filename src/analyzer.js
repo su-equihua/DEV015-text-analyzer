f96d2cb99de90f82b317d4a5d1934a430034634a
susana-equihua <susiequihua@gmail.com>
2024-05-28 23:15:39 -0600

    realicé dos funcionalidades correctas en analyzer; escrbí el codigo del manejo del DOM para acutualizar los valores de input en textarea y el boton de limpiar el contenido de la caja de texto son funcionales; modificaciones y pruebas en html para que los demas lenguajes respondieran; añadí estilo en el css

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,23 +1,40 @@
+//Analyzer es la parte de mi documento que tiene que ver con la manipulacion de los datos "parte interna del negocio"
 const analyzer = {  
-  
-  getWordCount: (text) => {
     
+  getWordCount: (text) => {   // el parametro de la funcion es text
+    //bloque de codigo de la funcion
+    const palabras = text.trim();  //metodo trim para eliminar los espacios al inicio y al final de la cadena
+    const words = palabras.split(/\s+/g)  /*hacer un arreglo (array) de subcadenas con split eliminando los espacios entre palabras
+    expresion regular (/\s+/g); g para que haga una busqueda global en toda la cadena */
+    console.log("conteo de palabras:",words); //Imprimir en la consola 
+    return words.length;    // la funcion retornará a la longitud de las palabras
+   
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   
   getCharacterCount: (text) => {
+    console.log("caractereres:", text.length)  //ver en la consola
+    return text.length   //length= longitud del texto; en este caso se hace un retorno para conocer la longitud del texto
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getCharacterCountExcludingSpaces: (text) => {
+    //const caracteresinespacio = 
+    
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    //if (typeof === 'number')  //si el texto es de tipo nombre
+    return 
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    
+
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
+//Index es la parte de mi documento que tiene que ver con toda la interacción de la usuaria con mi página
+//Se conecta con analyzer importándolo
 import analyzer from './analyzer.js';
 
 // para saber que está funcionando el js
 console.log("hola, si estoy conectado");
+//Utilizando selectores del DOM
 console.log(document.querySelector("textarea"));
 console.log(document.getElementById("reiniciar"));
 
-// Declaracion de la variable, en este caso es una constante usando DOM y usando un etiqueta HTML
-const textarea = document.querySelector("textarea").value;
 
-// Actualizar las métricas cuando se escriba en el cuadro de texto
-// Sintax de addEventListener(type, listener) es lo mismo addEventListener(event,function)
-/* Se utiliza un evento input por si el cambio del valor se da por pegar el texto sin 
-*limitarnos solo a la deteccion del texto por teclado (keydown) */
-
-textarea.addEventListener("input", function wordCount(){ //aquí van los parametros de la funcion
-    //bloque de codigo de la funcion
-const texto = string.trim();
-const palabras = string.plit(/\s+/)
-return palabras.length; // una funcion debe retornar a algo
-
-console.log("recuento:",palabras)  //para ver en la consola 
+// 1) Actualizar las métricas cuando se escriba en el cuadro de texto
+/*Declaracion de una variable constante; querySelector requiere llamar al elemento junto con su atributo de interés 
+('textarea[name="user-input"]); tambien se puede utilizar getElementBy */
+const textarea = document.querySelector('textarea[name="user-input"]');
+// Sintax de addEventListener(type, listener) = addEventListener(event,function)
+// Se utiliza un evento input para detectar el cambio del valor al escribir o pegar un texto; evento keydown lo detecta solo por teclado 
+/* event.target Se refiere al elemento que desencadenó un evento específico (es una referencia al objeto en el que se lanzó el evento)
+En este caso queremos el valor de input (campo de entrada) que el usario ingresó, se usa: event.target.valuea*/
+/*Quedaría asi: textarea.addEventListener("input",(event)=>{], pero nos limitaremos a usar event porque
+ ya está implicito (se tacha por default en la const, podemos no escribirla) */
+textarea.addEventListener("input",()=>{ 
+    //creamos una const que tenga con target.value
+    const text = event.target.value;
+    //impresion en la pantalla con la constante
+    console.log("El usario está escribiendo:", text);
+    //todas las metricas hechas en analyzer mostarán sus resultados con la constante text
+    //analyzer.getWordCount("conteo de palabras" + event.target.value)... de no haber hecho una constante quedaría así
+    analyzer.getWordCount(text)
+    analyzer.getCharacterCount(text);
+    analyzer.getCharacterCountExcludingSpaces(text);
+    analyzer.getNumberSum(text);
+    analyzer.getNumberCount(text);
+    analyzer.getAverageWordLength(text);
 });
 
+//2) Botón para limpiar el contenido de la caja de texto
+const btn = document.getElementById("reiniciar"); //se declara la constante
+btn.addEventListener("click", (limpiarContenido)=>{   //Se utiliza el manejador EventListener con el evento click
+document.querySelector('textarea[name="user-input"]').value = " ";  //Se hace un llamado al valor de input de mi texta y que al borrar el contenido lo deje en blanco " "
+console.log("se borró el contenido");
+});
 
-
-// Se hace una declaración por medio del Id
-const btn = document.getElementById("reiniciar");
-// Evento con add 
-btn.addEventListener("click",())
-
+// 3)Actualizar el atributo textContent del listado de metricas
+//const listado = document.getElementsByClassName ("metricas");
+//textContent = listado + analyzer.getCharacterCount; 
+//console.log("metricas",listado);
+//textContent = listado.getCharacterCount;
+//.value = "0";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
