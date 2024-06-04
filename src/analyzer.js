d02b5d10754ed79d1f8f8e036b5e7df820ada623
susana-equihua <susiequihua@gmail.com>
2024-06-03 22:21:31 -0600

    modificaciion de html con atributos data de acuerdo al readme; funcionalidades de analyzer e indexjs concluidas con explicación de cada codigo, ya no arroja errores; css en un 70%, aun hay que modificar, pre-termino del proyecto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,42 +1,104 @@
-//Analyzer es la parte de mi documento que tiene que ver con la manipulacion de los datos "parte interna del negocio"
+//Analyzer es la parte de mi documento que tiene que ver con la manipulacion de los datos 
 const analyzer = {  
-    
+      
   getWordCount: (text) => {   // el parametro de la funcion es text
     //bloque de codigo de la funcion
     const palabras = text.trim();  //metodo trim para eliminar los espacios al inicio y al final de la cadena
-    const words = palabras.split(/\s+/g)  /*hacer un arreglo (array) de subcadenas con split eliminando los espacios entre palabras
-    expresion regular (/\s+/g); g para que haga una busqueda global en toda la cadena */
-    console.log("conteo de palabras:",words); //Imprimir en la consola 
-    return words.length;    // la funcion retornará a la longitud de las palabras
+    const numPalabras = palabras.split(/\s+/g);  /*hacer un arreglo (array) de subcadenas con split eliminando los espacios entre palabras
+    *con una expresion regular (/\s+/g) que coincide con uno o mas caracteres de espacio e incluye los saltos de línea; 
+    g para que haga una busqueda global en toda la cadena */
+    //console.log("conteo de palabras:",numPalabras); //Imprimir en la consola 
+    return numPalabras.length;    //la funcion retornará a la longitud de las palabras
    
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   
   getCharacterCount: (text) => {
-    console.log("caractereres:", text.length)  //ver en la consola
-    return text.length   //length= longitud del texto; en este caso se hace un retorno para conocer la longitud del texto
+    //console.log("caractereres:", text.length)  //ver en la consola
+    return text.length   //length= longitud del texto; en este caso se hace un retorno para conocer la longitud de toda la cadena
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getCharacterCountExcludingSpaces: (text) => {
-    //const caracteresinespacio = 
+    //1)cómo quitar los espacios de una cadena string
+    /*se declara una constante con el metodo replace() ya que los string son datos primitivos inmutables asi que se 
+    *reemplazan los espacios en blanco. La expresion / significa que es una exprecion regular que busca espacios.
+    Regex de notacion literal \s para seleccionar espacios en blanco, tabulaciones y saltos de línea
+    +g para que sea haga en toda la cadena de string */
+    const caractereSinEspacio = text.replace(/\s+/g,"");
+
+    //Otra opcion para que no se cuenten los espacios  const caracteresinespacio = text.split("").join("");
+
+    /*2)Cómo quitar los signos de puntuacion... con el mismo metodo de replace
+    *pero con una regex que busque caracteres que no sean letras, numeros, acentos y la letra ñ
+    en una busqueda en toda la cadena g; se reemplazaran con "" (nada, es decir, que se eliminen)*/
+    const caracterSinEspacioSigno = caractereSinEspacio.replace(/[^a-zA-z0-9ÁÉÍÓÚáéíóúÑñ]/g,"");
+    //console.log("caracateres sin signo/espacio",caracterSinEspacioSigno.length);   //impresion de pantalla
+    return caracterSinEspacioSigno.length 
     
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
 
   getAverageWordLength: (text) => {    
+  //1)Separar la cadena string en subcadenas con los metodos trim y split para quitar los espacios, se obtiene un arreglo de subcadenas 
+  //2)Sacar el promedio de longitud del arreglo (lista); Promedio = suma de todos los elementos de la lista/ el numero de elementos de la lista
+    const palabraSinEspacio = text.trim(); //eliminar espacios al inicio y fin del texto para que no los cuente como caracteres de las palabras
+    const arregloPalabras = palabraSinEspacio.split(/\s+/g); //Se obtiene un arreglo de palabras (el metodo split siempre siempre devuelve un arrelgo)
+    //Hay que obtener el numero de caracteres por palabra, estos numeros se suman y se dividen por el numero de palabras (cadenas)
+    //Ciclo (bucle) "for" para que se cuenten los caracteres por palabra. Iterar el arreglo con un ciclo.
+    //console.log(arregloPalabras);
+   
+    let sumaCaracteres = 0  //Declarar la ariable de inicio del bucle en 0 para que se vaya guardando la suma de todas las palabras del arreglo
+   
+    for (let i = 0; i< arregloPalabras.length; i++) {  //la operacion se hará en todo el arreglo
+      sumaCaracteres += arregloPalabras[i].length      //Obtener el numero de caracateres de cada palabra del arreglo y se suman
+    }
+    //Declarar una variable constante para obtener el promedio.
+    const promedio = sumaCaracteres/arregloPalabras.length; //Promedio: dividir la suma de los caracteres por el numero de palabras en el texto
+    //Todas las funciones deben retornar/devolver algo
+    //return promedio.toFixed(1)  //Opcion 1) metodo toFixed para que solo me muestre un determinado numero de decimales
+    return Number (promedio.toFixed(2))  //Opción 2) metodo to Fixed sea parte de un objeto de tipo Number para que solo salgan dos decimales de ser necesario
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberCount: (text) => {
-    //if (typeof === 'number')  //si el texto es de tipo nombre
-    return 
+    //Encontrar todos los numeros de la cadena string 
+    //Contar la cantidad de número que hay
+    //Se utiliza el metodo match que es como un filtro que me va ayudar a encontrar los numeros que hay en la cadena string, devuelve un arreglo
+    //Regex: /\d/g para que busque todos los numeros coincidente de 0-9 individualmente
+    //const numeros = text.match(/0-9/g); esta forma no funcinó
+    //const numeros = text.match(/\d/g) //esta opción busca numeros enteros; los cuenta de manera individual  
+    //Esta expresión incluye numeros enteros y numeros con decimales despues del entero,los cuenta como palabras
+    const numeros =text.match(/\b\d+(\.\d+)?\b/g); //Constante que en la que se guardará el arreglo de numeros enteros y con decimales que se encuentre en la cadena de texto
+    // \b Asegura que el número esté delimitado por caracteres no alfanumérico 
+    if(numeros === null){   //Condicion: Si el valor del arreglo de numeros es nula que nos devuelva un valor 0 
+      return 0;  
+    }
+    else{   //pero si no es asi, entonces que nos devuelva el conteo de numeros que hay en el texto
+      return numeros.length;
+    }  
+    
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberSum: (text) => {
-    
-
+  //Obtener un arreglo que contenga los numeros en la cadena
+    //Convertir esos numeros tipo string a datos primitivos number
+    //Que cada numero se vaya sumando y de una suma total de todos los numeros que hay en el texto
+    //const numeros = text.match(/\d+(\.\d+)?/g) //no funcionó porque si cuenta los numeros que hay en palabras
+    const numeros = text.match(/\b\d+(\.\d+)?\b/g); //Constante para obtener el array (arreglo) de números de la cadena 
+    let sumaNumeros = 0;    //Declaracion de variable que inicie la suma en 0
+    if(numeros){   //condicionamos el código a modo de que si hay numeros en el arreglo se realice la función
+      for (let i = 0; i < numeros.length; i++) {   //Declaramos un bucle que va a recorrer todo el arreglo hasta el último índice del arreglo
+        sumaNumeros += parseFloat(numeros[i]);    //Cada iteración va a convertir el numero string en numero con parseFloat y lo va a sumar 
+      }
+      return sumaNumeros;  //Devolverá la suma total de cada numero que se encuentre en la cadena
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    }
+    else{
+      return 0  //si no se encuentran numeros entonces que devuelva 0
+    }
   },
-};
+}
+export default analyzer
 
-export default analyzer;
--- src/index.js
+++ src/index.js
@@ -3,50 +3,72 @@
 import analyzer from './analyzer.js';
 
 // para saber que está funcionando el js
-console.log("hola, si estoy conectado");
+//console.log("hola, si estoy conectado");
 //Utilizando selectores del DOM
-console.log(document.querySelector("textarea"));
-console.log(document.getElementById("reiniciar"));
+//console.log(document.querySelector("textarea"));
+//console.log(document.getElementById("reiniciar"));
 
 
-// 1) Actualizar las métricas cuando se escriba en el cuadro de texto
 /*Declaracion de una variable constante; querySelector requiere llamar al elemento junto con su atributo de interés 
 ('textarea[name="user-input"]); tambien se puede utilizar getElementBy */
 const textarea = document.querySelector('textarea[name="user-input"]');
+
+/*Declarar las constantes de cada metrica fuera de alguna función para poder ser utilizadas más de una vez 
+(boton y actualizacion de metricas)*/
+const palabras = document.querySelector('li[data-testid="word-count"]'); 
+const caracateres = document.querySelector('li[data-testid="character-count"]');
+const sinEspacioSignos = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const numeros = document.querySelector('li[data-testid="number-count"]');
+const sumaNumeros = document.querySelector('li[data-testid="number-sum"]');
+const longitud = document.querySelector('li[data-testid="word-length-average"]');
+
+
+// Funcion: Actualizar las métricas cuando se escriba en el cuadro de texto
 // Sintax de addEventListener(type, listener) = addEventListener(event,function)
-// Se utiliza un evento input para detectar el cambio del valor al escribir o pegar un texto; evento keydown lo detecta solo por teclado 
-/* event.target Se refiere al elemento que desencadenó un evento específico (es una referencia al objeto en el que se lanzó el evento)
-En este caso queremos el valor de input (campo de entrada) que el usario ingresó, se usa: event.target.valuea*/
-/*Quedaría asi: textarea.addEventListener("input",(event)=>{], pero nos limitaremos a usar event porque
+textarea.addEventListener("input",function(){ 
+//creamos una const con target.value que nos dará el valor de input
+  const text = event.target.value;  
+  //Se utiliza un evento input para detectar el cambio del valor al escribir o pegar un texto; evento keydown lo detecta solo por teclado
+  /* event.target Se refiere al elemento que desencadenó un evento específico (es una referencia al objeto en el que se lanzó el evento)
+En este caso queremos el valor de input (campo de entrada) que el usario ingresó, se usa: event.target.valuea o textarea.value*/
+  /*Quedaría asi: textarea.addEventListener("input",(event)=>{], pero nos limitaremos a usar event porque
  ya está implicito (se tacha por default en la const, podemos no escribirla) */
-textarea.addEventListener("input",()=>{ 
-    //creamos una const que tenga con target.value
-    const text = event.target.value;
-    //impresion en la pantalla con la constante
-    console.log("El usario está escribiendo:", text);
-    //todas las metricas hechas en analyzer mostarán sus resultados con la constante text
-    //analyzer.getWordCount("conteo de palabras" + event.target.value)... de no haber hecho una constante quedaría así
-    analyzer.getWordCount(text)
-    analyzer.getCharacterCount(text);
-    analyzer.getCharacterCountExcludingSpaces(text);
-    analyzer.getNumberSum(text);
-    analyzer.getNumberCount(text);
-    analyzer.getAverageWordLength(text);
+
+
+  //impresion en la pantalla con la constante
+  //console.log("El usario está escribiendo:", text);
+
+  //Funcion: Actualizar con el atributo textContent el resultado del listado de metricas
+  //Las metricas hechas en analyzer mostrarán sus resultados con la constante text
+  //analyzer.getWordCount(event.target.value)... de no haber hecho una constante quedaría así
+  palabras.textContent = "Palabras:" + " "+ analyzer.getWordCount(text); 
+  /*Se utiliza el atributo textConten con la constante anteriormente construida, el texto de cada metrica
+ + el valor obtenido con el metodo correspondiente de analyzer*/
+
+  caracateres.textContent = "Caracteres:" + " " + analyzer.getCharacterCount(text);
+  sinEspacioSignos.textContent = "Caracteres sin espacio:" + " " + analyzer.getCharacterCountExcludingSpaces(text);
+  numeros.textContent = "Números:" + " " + analyzer.getNumberCount(text);
+  sumaNumeros.textContent = "Suma de números:" + " " + analyzer.getNumberSum(text);
+  longitud.textContent = "Longitud promedio:" + " " + analyzer.getAverageWordLength(text);
+}); 
+
+//Funcion: Botón para limpiar el contenido de la caja de texto
+//se declara la constante
+const btn = document.getElementById("reiniciar"); 
+//Se utiliza el manejador EventListener con el evento click
+btn.addEventListener("click", ()=>{   
+/*Se hace un llamado al valor de input de mi texta y que al borrar el contenido lo deje en blanco "" 
+(no dejar espacio entre las comillas porque actualiza la caja con ese espacio)*/
+  document.querySelector('textarea[name="user-input"]').value = "";  
+  //console.log("se borró el contenido"); //impresion en pantalla
+
+  //Ordenamos que se actualice en valor 0 cada metrica utlizando textConten con cada una de las const
+  palabras.textContent = "Palabras: ";
+  caracateres.textContent = "Caracteres: "; 
+  sinEspacioSignos.textContent = "Caracteres sin espacio: "; 
+  numeros.textContent = "Números: "; 
+  sumaNumeros.textContent = "Suma de números: ";
+  longitud.textContent = "Longitud promedio: ";
+
 });
-
-//2) Botón para limpiar el contenido de la caja de texto
-const btn = document.getElementById("reiniciar"); //se declara la constante
-btn.addEventListener("click", (limpiarContenido)=>{   //Se utiliza el manejador EventListener con el evento click
-document.querySelector('textarea[name="user-input"]').value = " ";  //Se hace un llamado al valor de input de mi texta y que al borrar el contenido lo deje en blanco " "
-console.log("se borró el contenido");
-});
-
-// 3)Actualizar el atributo textContent del listado de metricas
-//const listado = document.getElementsByClassName ("metricas");
-//textContent = listado + analyzer.getCharacterCount; 
-//console.log("metricas",listado);
-//textContent = listado.getCharacterCount;
-//.value = "0";
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-
